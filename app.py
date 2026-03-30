7ccce0ba0af50c639700f1d86292b3120c4133c8
sk-sln <82103847+sk-sln@users.noreply.github.com>
2026-03-30 21:52:38 +0200

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def run_web():
 
 # --- НАСТРОЙКИ ---
 # ВАЖНО: Проверь, что это последняя ссылка из Deploy в GAS
-GAS_URL = "https://script.google.com/macros/s/XXXXXXXXX/exec" 
+GAS_URL = "https://script.google.com/macros/s/AKfycbxulwXBqzuxXygyKy-HFvoRJJlos7SgN1HExVrNDhMyTpUnmHE_EA_GXaXUlv3D4_pSuA/exec" 
 
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
